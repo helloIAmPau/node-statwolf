68f3993d547c569dc89e9c248cb6e49142fa1754
Pasquale Boemio <pasquale.boemio@statwolf.com>
2017-08-23 09:02:29 +0100

    updates

--- packages/statwolf-debugger-client/lib/index.js
+++ packages/statwolf-debugger-client/lib/index.js
@@ -14,6 +14,10 @@ module.exports = function(addr) {
     _send({ type: 'join', body: { room: 'debug' } });
   });
 
+  client.on('close', function() {
+    proxy.emit('close');
+  });
+
   client.on('message', function(msg = '{}') {
     msg = JSON.parse(msg);
     msg.body = JSON.parse(msg.body);
@@ -28,13 +32,37 @@ module.exports = function(addr) {
   });
 
   proxy.dbgr = function(id) {
-    const dbgr = {};
+    const dbgr = {
+      id
+    };
+
+    dbgr.continue = function() {
+      _send({ type: 'debug:command', body: { debugger: id, command: 'continue' } });
+    };
+
+    dbgr.stepNext = function() {
+      _send({ type: 'debug:command', body: { debugger: id, command: 'stepNext' } });
+    };
+
+    dbgr.stepIn = function() {
+      _send({ type: 'debug:command', body: { debugger: id, command: 'stepIn' } });
+    };
+
+    dbgr.stepOut = function() {
+      _send({ type: 'debug:command', body: { debugger: id, command: 'stepOut' } });
+    };
 
     dbgr.evaluate = function(code, scope) {
       _send({ type: 'debug:command', body: { code, scope, debugger: id, command: 'eval' } });
     };
 
+    _send({ type: 'debug:command', body: { debugger: id, command: 'status' } });
+
     return dbgr;
+  };
+
+  proxy.close = function() {
+    client.close();
   };
 
   return proxy;
