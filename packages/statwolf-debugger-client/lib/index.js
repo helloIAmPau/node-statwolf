7a3afc66df0253cac6f72aeb94eec1dc36e6944e
Pasquale Boemio <pasquale.boemio@statwolf.com>
2017-09-08 12:40:51 +0100

    added fix

--- packages/statwolf-debugger-client/lib/index.js
+++ packages/statwolf-debugger-client/lib/index.js
@@ -4,66 +4,129 @@ const EventEmitter = require('events');
 module.exports = function(addr) {
   const proxy = new EventEmitter();
 
-  const client = new WebSocket(addr);
+  try {
+    const client = new WebSocket(addr);
 
-  const _send = function(msg) {
-    client.send(JSON.stringify({ type: msg.type, body: JSON.stringify(msg.body) }));
-  };
-
-  client.on('open', function() {
-    _send({ type: 'join', body: { room: 'debug' } });
-  });
-
-  client.on('close', function() {
-    proxy.emit('close');
-  });
-
-  client.on('message', function(msg = '{}') {
-    msg = JSON.parse(msg);
-    msg.body = JSON.parse(msg.body);
-
-    if(msg.type === 'debug:list') {
-      proxy.emit('list', msg.body);
-
-      return;
-    }
-
-    proxy.emit(msg.type, msg.body);
-  });
-
-  proxy.dbgr = function(id) {
-    const dbgr = {
-      id
+    const _send = function(msg) {
+      client.send(JSON.stringify({
+        type: msg.type,
+        body: JSON.stringify(msg.body)
+      }));
     };
 
-    dbgr.continue = function() {
-      _send({ type: 'debug:command', body: { debugger: id, command: 'continue' } });
+    client.on('open', function() {
+      console.log('triggered');
+      _send({
+        type: 'join',
+        body: {
+          room: 'debug'
+        }
+      });
+    });
+
+    client.on('close', function() {
+      proxy.emit('close');
+    });
+
+    client.on('message', function(msg = '{}') {
+      msg = JSON.parse(msg);
+      msg.body = JSON.parse(msg.body);
+
+      if (msg.type === 'debug:list') {
+        proxy.emit('list', msg.body);
+
+        return;
+      }
+
+      proxy.emit(msg.type, msg.body);
+    });
+
+    proxy.dbgr = function(id) {
+      const dbgr = {
+        id
+      };
+
+      dbgr.continue = function() {
+        _send({
+          type: 'debug:command',
+          body: {
+            debugger: id,
+            command: 'continue'
+          }
+        });
+      };
+
+      dbgr.stepNext = function() {
+        _send({
+          type: 'debug:command',
+          body: {
+            debugger: id,
+            command: 'stepNext'
+          }
+        });
+      };
+
+      dbgr.stepIn = function() {
+        _send({
+          type: 'debug:command',
+          body: {
+            debugger: id,
+            command: 'stepIn'
+          }
+        });
+      };
+
+      dbgr.stepOut = function() {
+        _send({
+          type: 'debug:command',
+          body: {
+            debugger: id,
+            command: 'stepOut'
+          }
+        });
+      };
+
+      dbgr.evaluate = function(code, scope) {
+        _send({
+          type: 'debug:command',
+          body: {
+            code,
+            scope,
+            debugger: id,
+            command: 'eval'
+          }
+        });
+      };
+
+      _send({
+        type: 'debug:command',
+        body: {
+          debugger: id,
+          command: 'status'
+        }
+      });
+
+      return dbgr;
     };
 
-    dbgr.stepNext = function() {
-      _send({ type: 'debug:command', body: { debugger: id, command: 'stepNext' } });
+    proxy.close = function() {
+      if (client.readyState === client.OPEN) {
+        client.close();
+      } else if (client.readyState === client.CONNECTING) {
+        //deferring close callback
+        client.removeAllListeners('open');
+        client.once('open', function() {
+          if (client.readyState === client.OPEN) {
+            client.close();
+          }
+        });
+      }
     };
 
-    dbgr.stepIn = function() {
-      _send({ type: 'debug:command', body: { debugger: id, command: 'stepIn' } });
-    };
+    return proxy;
+  } catch (e) {
+    console.log(e.stack);
 
-    dbgr.stepOut = function() {
-      _send({ type: 'debug:command', body: { debugger: id, command: 'stepOut' } });
-    };
-
-    dbgr.evaluate = function(code, scope) {
-      _send({ type: 'debug:command', body: { code, scope, debugger: id, command: 'eval' } });
-    };
-
-    _send({ type: 'debug:command', body: { debugger: id, command: 'status' } });
-
-    return dbgr;
-  };
-
-  proxy.close = function() {
-    client.close();
-  };
-
-  return proxy;
+    throw new Error(`Unable to connect to ${ addr }`);
+  }
 };
