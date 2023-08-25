3990867e76954bd54cb9b7d3494c291ecc678c4a
Pasquale Boemio <boemianrapsodi@gmail.com>
2023-08-25 14:45:53 +0200

    fixes

--- modules/@statwolf/code/path.js
+++ modules/@statwolf/code/path.js
@@ -1,6 +1,6 @@
 export default function(path) {
     if(process.platform === "win32") {
-      return `${ s[0] = s[0].toUpperCase() }${ s.slice(1) }`;
+      return `${ path[0] = path[0].toUpperCase() }${ path.slice(1) }`;
     }
 
     return path;
--- modules/@statwolf/statwolf/services/bundle.js
+++ modules/@statwolf/statwolf/services/bundle.js
@@ -1,6 +1,7 @@
 import { resolve } from 'path';
 import http from '../http';
 import compile from '../compile';
+import exec from './exec';
 
 export default function({ input, project, host, drop }) {
   const basePath = resolve(project);
@@ -11,6 +12,15 @@ export default function({ input, project, host, drop }) {
     return client('Publish', {
       delete_all: drop,
       changes
+    });
+  }).then(function(result) {
+    const code = 'var key = `lastResourceUpdate_${$user.changesetName()}`; var res = $environment().set({ [key]: new Date() }); console.log(`Updated: ${ res }`); return res;';
+ 
+    return exec({
+      code,
+      host
+    }).then(function() {
+      return result;
     });
   });
 };
